2d477baecc54b14dcface40945d1a225422055aa
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-06-12 19:24:56 +0530

    REFACTORED: Autowired to no need to create object

--- src/main/java/org/intellect/car/Corolla.java
+++ src/main/java/org/intellect/car/Corolla.java
@@ -2,14 +2,17 @@ package org.intellect.car;
 
 import org.intellect.interfaces.Car;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Component;
-
-import com.sun.xml.internal.ws.api.pipe.Engine;
-
 @Component("corolla")
 public class Corolla implements Car {
-
+	
+	//Using Autowired no need to create object
+	@Autowired
+	Engine engine;
+	
 	public String specs() {
-		return "Hatchback from Suzuki";
+		String spec = "sadan from toyota with engine type :"+engine.type;
+		return spec;
 	}
 }
